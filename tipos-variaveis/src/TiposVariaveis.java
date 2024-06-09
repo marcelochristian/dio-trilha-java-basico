0e0145c86830d14de7ae8be321a5bf5a1c6beb33
marcelochristian <marcelo.cristian@yahoo.com.br>
2024-06-09 10:52:18 -0300

    tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,6 +2,17 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         int idade;
         int anoFabricacao = 2021;
-        double salarioMinimo = 2500;
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
