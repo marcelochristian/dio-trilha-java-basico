2d2472afdcd8b73f5282c4be99a3744072b5c4e9
marcelochristian <marcelo.cristian@yahoo.com.br>
2024-06-09 10:32:34 -0300

    Tipos e Variaveis - 3

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,7 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int idade;
+        int anoFabricacao = 2021;
+        double salarioMinimo = 2500;
     }
 }
